b399864c59ca75e61776450023eaf8d6a1b82141
Chiel92 <ctje92@gmail.com>
2013-09-14 20:23:50 +0200

    This is already something, but passing zero arguments to count still doesn't work

--- 2.5.hs
+++ 2.5.hs
@@ -1,8 +1,20 @@
 module A2_5 where
 
-test :: [Int]
-test = [count, count 1 2 3, count "" [True, False] id (+)]
+--test :: [Int]
+--test = [count 1, count 1 2 3, count "" [True, False] id (+)]
 
-count = \x -> case x of
-     () -> 0
-     otherwise -> count
+--count = \x -> case x of
+--     () -> 0
+--     otherwise -> count
+
+class CountArgs r where
+    count :: a -> r
+
+instance CountArgs Int where
+    count = \x -> 0
+
+instance (CountArgs r) => CountArgs (a -> r) where
+    count x = count
+
+test' :: [Int]
+test' = [count 1, count 1 2 3, count "" [True, False] id (+)]
