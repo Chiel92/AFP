d9763458718dfa269c6b88bd84655976ce9d06a2
Chiel92 <ctje92@gmail.com>
2013-09-30 16:19:13 +0200

    Some minor consistency fixes. TODO: reject programs that require non-existing stack elements during type checking.

--- Set2/A2_9.hs
+++ Set2/A2_9.hs
@@ -3,7 +3,7 @@ module A2_9 where
 -- A nice abbreviation
 type Stack = [Int]
 
--- Some test expressions
+-- Some tests
 p0, p1, p2, p3 :: Int
 p0 = start store 1 stop
 p1 = start store 3 store 5 add stop
@@ -11,23 +11,22 @@ p2 = start store 3 store 6 store 2 mul add stop
 p3 = start store 2 add stop
 
 -- The start function
-start :: (Stack -> r) -> r
-start r = r []
+start :: (Stack -> k) -> k
+start k = k []
 
 -- The store function
-store :: Stack -> Int -> (Stack -> r) -> r
-store s n r = r (n:s)
+store :: Stack -> Int -> (Stack -> k) -> k
+store s n k = k (n:s)
 
 -- The add function
-add :: Stack -> (Stack -> r) -> r
-add (x:y:ys) r = r (x + y : ys)
+add :: Stack -> (Stack -> k) -> k
+add (x:y:s) k = k (x + y : s)
 
 -- The mul function
-mul :: Stack -> (Stack -> r) -> r
-mul (x:y:ys) r = r (x * y : ys)
+mul :: Stack -> (Stack -> k) -> k
+mul (x:y:s) k = k (x * y : s)
 
 -- The stop function
 stop :: Stack -> Int
-stop []     = 0
-stop (x:xs) = x
+stop (x:s) = x
 
