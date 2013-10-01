6176c21b37c2f10594fe42b583b7e149660164a1
Mattias <matheiusb@gmail.com>
2013-10-01 17:44:42 +0200

    Some more gibberish with the name of 4.1, and made the  multiplication multiplicate.

--- Set2/A4_1.hs
+++ Set2/A4_1.hs
@@ -6,9 +6,14 @@ data F a = F (F a -> a)
 unF :: F a -> F a -> a
 unF (F f) = f
 
--- y f = (\x -> f x x) (\x -> f x x)
-y' (F f) = 
+-- y f = (\x -> f (x x)) (\x -> f (x x))
+
+-- y is a function, that gets one parameter: f          - WRONG - it gets more than 1 parameter, it gets x as well, or something :S
+-- f is a function, that gets one parameter: (x x)      - Except that that thing is a function again, so it should get more parameters...
+-- x is a function, that gets one parameter: x (itself) - Which is, of course, a function...
 
 -- y = \f -> (\x -> f (x x)) (\x -> f (x x))
--- y f = let g x = f x x in g g
+-- y f = let g x = f (x x) in g g
+
+y f = g g where g x = f (x x)
 
