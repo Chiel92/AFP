db2d9bd66476e5576f5348ab71220c4cf0b56d6e
Chiel92 <ctje92@gmail.com>
2013-10-02 11:41:50 +0200

    some polishing

--- Set2/A2_9.hs
+++ Set2/A2_9.hs
@@ -1,71 +1,64 @@
 module A2_9 where
 
+-- The naieve solution
+
 -- A nice abbreviation
 type Stack = [Int]
 
--- Some tests
+-- Implementation of the stack functions
+start :: (Stack -> k) -> k
+start k = k []
+
+store :: Stack -> Int -> (Stack -> k) -> k
+store s n k = k (n:s)
+
+add :: Stack -> (Stack -> k) -> k
+add (x:y:s) k = k (x + y : s)
+
+mul :: Stack -> (Stack -> k) -> k
+mul (x:y:s) k = k (x * y : s)
+
+stop :: Stack -> Int
+stop (x:s) = x
+
+
+-- Tests
 p0, p1, p2, p3 :: Int
 p0 = start store 1 stop
 p1 = start store 3 store 5 add stop
 p2 = start store 3 store 6 store 2 mul add stop
 p3 = start store 2 add stop
 
--- The start function
-start :: (Stack -> k) -> k
-start k = k []
-
--- The store function
-store :: Stack -> Int -> (Stack -> k) -> k
-store s n k = k (n:s)
-
--- The add function
-add :: Stack -> (Stack -> k) -> k
-add (x:y:s) k = k (x + y : s)
-
--- The mul function
-mul :: Stack -> (Stack -> k) -> k
-mul (x:y:s) k = k (x * y : s)
-
--- The stop function
-stop :: Stack -> Int
-stop (x:s) = x
-
 
 -- The solution that rejects programs that require
 -- nonexisting stack elements during type checking
 data Stack' s = Stack' s Int deriving Show
-data Empty = Empty
+data EmptyStack' = EmptyStack'
 
--- example usage for Stack'
-x = Empty
-y = Stack' x 2
-z = Stack' y 5
+-- Implementation of the stack functions
+start' :: (EmptyStack' -> r) -> r
+start' k = k EmptyStack'
 
--- Some tests
---p0', p1', p2' :: Int
-p0' = start' store' 1 stop'
-p1' = start' store' 3 store' 5 add' stop'
-p2' = start' store' 3 store' 6 store' 2 mul' add' stop'
---p3' :: Int
---p3' = start' store' 2 add' stop'
-
--- The start function
-start' :: (Empty -> r) -> r
-start' k = k Empty
-
--- The store function
 store' :: s -> Int -> (Stack' s -> k) -> k
 store' s n k = k (Stack' s n)
 
--- The add function
 add' :: Stack' (Stack' s) -> (Stack' s -> k) -> k
 add' (Stack' (Stack' s x) y) k = k (Stack' s (x + y))
 
--- The mul function
 mul' :: Stack' (Stack' s) -> (Stack' s -> k) -> k
 mul' (Stack' (Stack' s x) y) k =  k (Stack' s (x * y))
 
--- The stop function
 stop' :: Stack' s -> Int
 stop' (Stack' s n) = n
+
+
+-- Tests
+p0', p1', p2' :: Int
+p0' = start' store' 1 stop'
+p1' = start' store' 3 store' 5 add' stop'
+p2' = start' store' 3 store' 6 store' 2 mul' add' stop'
+
+-- As required p3' doesn't compile
+-- p3' :: Int
+-- p3' = start' store' 2 add' stop'
 
