feb35a3005061c1ecaa03e86a86dde386295bdcb
Chiel92 <ctje92@gmail.com>
2013-10-02 11:34:30 +0200

    yay!!111!!!11!

--- Set2/A2_9.hs
+++ Set2/A2_9.hs
@@ -33,24 +33,28 @@ stop (x:s) = x
 
 -- The solution that rejects programs that require
 -- nonexisting stack elements during type checking
-data Stack' s = Stack' s Int | Empty deriving Show
+data Stack' s = Stack' s Int deriving Show
+data Empty = Empty
+
+-- example usage for Stack'
 x = Empty
 y = Stack' x 2
 z = Stack' y 5
 
 -- Some tests
-p0', p1', p2', p3' :: Int
+--p0', p1', p2' :: Int
 p0' = start' store' 1 stop'
 p1' = start' store' 3 store' 5 add' stop'
 p2' = start' store' 3 store' 6 store' 2 mul' add' stop'
-p3' = start' store' 2 add' stop'
+--p3' :: Int
+--p3' = start' store' 2 add' stop'
 
 -- The start function
-start' :: (Stack' s -> k) -> k
+start' :: (Empty -> r) -> r
 start' k = k Empty
 
 -- The store function
-store' :: Stack' s -> Int -> (Stack' (Stack' s) -> k) -> k
+store' :: s -> Int -> (Stack' s -> k) -> k
 store' s n k = k (Stack' s n)
 
 -- The add function
