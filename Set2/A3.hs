5f8ac8db6e094f89a57dd6e08709b781b0bcb57b
Chiel92 <ctje92@gmail.com>
2013-10-03 18:31:26 +0200

    some initial stuff added for feature 3. Ga nu eten

--- Set2/A3.hs
+++ Set2/A3.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies #-}
 module A3 where
 import Control.Monad.State
 import Data.List
@@ -35,11 +35,18 @@ instance Show (StateMonadPlus s String) where
         g (Right (a, _, _)) = show a
         g (Left s) = s
 
+-- Allow saving the current state, and restoring a previous state as the current state
+class MonadState s m => StoreState s m | m -> s where
+    saveState :: m ()
+    loadState :: m ()
+
 
 
 -- This function should count the number of binds (>>=)
 -- and returns (and other primitive functions) that have been encountered,
 -- including the call to diagnostics at hand.
+-- The functions for Features 2 and 3, as well as get and put,
+-- should also be part of the diagnosis.
 diagnostics :: StateMonadPlus s String
 diagnostics = StateMonadPlus (\(s, d) ->
               let d'       = incDict "diagnostics" d
@@ -53,18 +60,13 @@ incDict key []                      = [(key, 1)]
 incDict key ((k, v):xs) | k == key  = (k, v + 1):xs
                         | otherwise = (k, v):incDict key xs
 
--- Secondly, provide a function annotate that
--- allows a user to annotate a computation with a given label.
--- The functions for
--- Features 2 and 3, as well as get and put,
--- should also be part of the diagnosis.
+-- Allow a user to annotate a computation with a given label.
 annotate :: String -> StateMonadPlus s a -> StateMonadPlus s a
 annotate key m = StateMonadPlus (\(s, d) ->
                  let  Right (a, s', d') = runStateMonadPlus m (s, d)
                       d''               = incDict key d'
                  in Right (a, s', d''))
 
--- Running the monad.
 -- Given a computation in the StateMonadPlus and an initial
 -- state, runStateMonadPlus returns either an error message
 -- if the computation failed, or
@@ -86,6 +88,17 @@ test2 = do
 
 test3 = do
     annotate "A" (return 3 >> return 4)
-    fail "bla"
+    fail "We failed, but didn't get an exception"
+    annotate "A" (return 3 >> return 4)
     return 5
     diagnostics
+
+-- test4 = do i1 <- get; saveState
+--     modify (*2)
+--     i2 <- get; saveState
+--     modify (*2)
+--     i3 <- get; loadState
+--     i4 <- get; loadState
+--     i5 <- get
+--     return (i1, i2, i3, i4, i5)
+
