2b0a1e01cf1a50c5330548d9b050ff16ca68acdf
Chiel92 <ctje92@gmail.com>
2013-10-03 10:26:46 +0200

    small changes in 4.2

--- Set2/A4_2.hs
+++ Set2/A4_2.hs
@@ -1,15 +1,11 @@
 module A4_2 where
 
--- a nested datatype for square matrices
+-- A nested datatype for square matrices
+-- We use row vectors, because that reads conveniently in the code
 type Square a    = Square' Nil a
 data Square' t a = Zero (t (t a)) | Succ (Square' (Cons t) a)
 data Nil a       = Nil deriving Show
 data Cons t a    = Cons a (t a) deriving Show
-
--- The 1D identity matrix
-i1 :: Square Int
-i1 = Succ (Zero (Cons e1 Nil)) where
-    e1 = Cons 1 Nil
 
 -- The 2D identity matrix
 i2 :: Square Int
@@ -19,8 +15,8 @@ i2 = Succ (Succ (Zero (Cons e1 (Cons e2 Nil)))) where
 
 -- The 123456789 matrix :)
 m3 :: Square Int
-m3 = Succ (Succ (Succ (Zero (Cons v1 (Cons v2 (Cons v3 Nil)))))) where
-    v1 = Cons 1 (Cons 4 (Cons 7 Nil))
-    v2 = Cons 2 (Cons 5 (Cons 8 Nil))
-    v3 = Cons 3 (Cons 6 (Cons 9 Nil))
+m3 = Succ (Succ (Succ (Zero (Cons r1 (Cons r2 (Cons r3 Nil)))))) where
+    r1 = Cons 1 (Cons 2 (Cons 3 Nil))
+    r2 = Cons 4 (Cons 5 (Cons 6 Nil))
+    r3 = Cons 7 (Cons 8 (Cons 9 Nil))
 
