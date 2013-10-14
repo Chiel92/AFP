ca8fc44d28b79396a07297c85138fc36941f39c4
Mattias <matheiusb@gmail.com>
2013-10-14 17:20:54 +0200

    Added text to 8.3, as required. It now actually is finished, yay!

--- Set3/A8_3.hs
+++ Set3/A8_3.hs
@@ -2,13 +2,27 @@ module A8_3 where
 
 -- You gotta love hacks
 forceBoolList :: [Bool] -> r -> r
-forceBoolList xs k = if length (fbl xs) == 42 then k else k where
-    fbl []     = []
-    fbl (x:xs) |x==True  = True:fbl xs
-               |x==False = False:fbl xs
+forceBoolList xs k = if length (forceBoolList' xs) == 42 then k else k
+
+-- A version of forceBoolList that has type [Bool] to [Bool]
+forceBoolList' :: [Bool] -> [Bool]
+forceBoolList' []                = []
+forceBoolList' (x:xs)|x == True  = True:forceBoolList' xs
+                     |x == False = False:forceBoolList' xs
 
 -- Warning, the following code might crash.
 test = do
-    two <- return $ forceBoolList [True, False, undefined, True] (1+1)
+    two <- return $ forceBoolList [True, 1337 > 9001, undefined, True] (1+1)
     print two
+
+test' = do
+    list <- return $ forceBoolList' [True, 1337 > 9001, undefined, True]
+    print $ length list
+
+-- The reason why you might want to use [Bool] -> r -> r is because seq does so too.
+-- You can use it on the fly and be sure it evaluates while doing something else.
+-- You can however define a forceBoolList' so that it has type [Bool] -> [Bool]
+-- and still evaluates the list.
+-- force a = seq a a is useless, because it only evaluates the list,
+-- and not the boolean expressions inside.
 
