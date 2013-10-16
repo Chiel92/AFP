cf72a81d2dc9a19b5d0f52604e3ed9525924a4b8
Mattias <matheiusb@gmail.com>
2013-10-16 15:00:35 +0200

    Added explanation and other good-to-have text

--- Set3/A8_5.hs
+++ Set3/A8_5.hs
@@ -1,24 +1,21 @@
 module A8_3 where
 
--- This example is copied from the internet:
--- https://www.google.nl/search?q=haskell+exponentiol+type
+-- This example is found on the internet:
 -- http://www.mail-archive.com/haskell-cafe@haskell.org/msg65869.html
 
+-- It works because the duplicate funtion duplicates the type signature (the amount of a's in the type signature doubles every time you 'add another dup function').
+-- The amount of a's in the type signature is 2^n, where n is the number of composed dub's, which is exponential..
+-- Because this doesn't only duplicate the number of a's, but also wraps it in different tuples every time, this is a way to create a truly large type signature :)
+
+dup :: a -> (a, a)
 dup x = (x,x)
 
 test1  = dup
 test2  = dup.dup
-test3  = dup.dup.dup
-test6  = dup.dup.dup.dup.dup.dup
-test9  = dup.dup.dup.dup.dup.dup.dup.dup.dup
+test4  = dup.dup.dup.dup
+test8  = dup.dup.dup.dup.dup.dup.dup.dup
 test12 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup
--- test24 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.
--- test48 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dupdup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dupdup.dup.dup.dup
-
--- TODO: an explanation why it's exponential (like:
---       This is exponential because the amount of a's is 2^n, and thus the time it takes to type check this is 2^n, which is exponential
--- )
--- Another TODO is to try this idea with different types - (the truly large type :))
-
--- P.S. test24 is horrible on ghci, but maybe it's not horrible enough when you actually compile it - for the ones who volunteer to try that, I've made a test48, just to be sure :)
+-- test16 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup
+-- test32 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup
+-- test64 = dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup.dup
 
